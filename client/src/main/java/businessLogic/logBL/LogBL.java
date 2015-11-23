e8b3c098681ae5a1d3cefd2b98362cf733eb3164
wserious <948436185@qq.com>
2015-11-23 19:31:27 +0800

    change in LogBL&LogController

--- client/src/main/java/businessLogic/logBL/LogBL.java
+++ client/src/main/java/businessLogic/logBL/LogBL.java
@@ -1,11 +1,67 @@
 package businessLogic.logBL;
 
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
+import client.ClientInitException;
+import client.RMIHelper;
+import dataService.LogDataService;
 import po.LogPO;
+import vo.LogVO;
 
 public class LogBL {
-	public List<LogPO> getLogList(){
+	LogDataService logDS;
+	List<LogPO> logList; 
+	
+	public  LogBL() throws RemoteException {
+		try {
+			RMIHelper.initLogDataService();
+			logDS = RMIHelper.getLogDataService();
+			logList=logDS.getLogList();
+		} catch (ClientInitException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public List<LogVO> getAllLogList(){
+		List<LogVO> result=new ArrayList<LogVO>();
+		for(LogPO temp : logList){
+			result.add(new LogVO(temp));
+		}
+		return result;
+	}
+	
+	public List<LogVO> getLogOfDate(Date d){
+		List<LogVO> result = new ArrayList<LogVO>();
+		
+		for(LogPO temp : logList){
+			if(temp.getD()==d){
+				result.add(new LogVO(temp));
+			}
+		}
+		return result;
+		
+	}
+	
+	public List<LogVO> getLogOfUser(String userID){
+		List<LogVO> result = new ArrayList<LogVO>();
+		
+		for(LogPO temp : logList){
+			if(temp.getIdentityID()==userID){
+				result.add(new LogVO(temp));
+			}
+		}
+		return result;
+	}
+	
+	public String add(LogVO vo) throws RemoteException{
+		LogPO po=vo.transToPO();
+		logList.add(0,po);
+		logDS.add(po);
+		
 		return null;
 	}
+	
 }
--- client/src/main/java/businessLogic/logBL/LogController.java
+++ client/src/main/java/businessLogic/logBL/LogController.java
@@ -1,13 +1,34 @@
 package businessLogic.logBL;
 
+import java.rmi.RemoteException;
+import java.util.Date;
 import java.util.List;
 
 import vo.LogVO;
 import businessLogicService.logBLService.LogBLService;
 
 public class LogController implements LogBLService {
-
-
+	LogBL logBL;
+	public LogController() throws RemoteException{
+		logBL= new LogBL();
+	}
+	
+	public List<LogVO> getAllLogList(){
+		return logBL.getAllLogList();
+	}
+	
+	public List<LogVO> getLogListOfDate(Date d){
+		return logBL.getLogOfDate(d);
+	}
+	
+	public List<LogVO> getLogListOfUser(String userID){
+		return logBL.getLogOfUser(userID);
+	}
+	
+	public String add(LogVO vo) throws RemoteException{
+		return logBL.add(vo);
+	}
+	
 	public List<LogVO> getLogList(){
 		return null;
 	}
