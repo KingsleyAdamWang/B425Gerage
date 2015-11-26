16115d774b68fd7965d8711bbd993c54fab1bc8c
wserious <948436185@qq.com>
2015-11-27 00:56:04 +0800

    no message

--- client/src/main/java/businessLogic/manageBL/StrategyBL.java
+++ client/src/main/java/businessLogic/manageBL/StrategyBL.java
@@ -16,92 +16,96 @@ import enumSet.PackType;
 public class StrategyBL {
 	private PriceConstDataService pcDS;
 	private PriceConstPO pcPO;
-	public  StrategyBL() throws RemoteException {
+
+	public StrategyBL() throws RemoteException {
 		try {
-				RMIHelper.initPriceConstDataService();
+			RMIHelper.initPriceConstDataService();
 			pcDS = RMIHelper.getPriceConstDataService();
-			pcPO=pcDS.getPriceConst();
+			pcPO = pcDS.getPriceConst();
 
 		} catch (ClientInitException e) {
 			e.printStackTrace();
 		}
 	}
-	
-	public List<String> getCities(){
+
+	public List<String> getCities() {
 		List<String> cities = new ArrayList<String>();
-		List<Distance> distances=pcPO.getDistances();
-		int label1=0;//label表示状态，0表示未找到相同，1表示已找到相同
-		int label2=0;
-		for(Distance temp: distances){
-			label1=0;
-			label2=0;
-			String city1= temp.getCity1();
-			String city2=temp.getCity2();
-			for(String tempCity: cities){
-				if(tempCity.equals(city1)){
-					//如果找到相同的城市，label设置为1，跳出
-					label1=1;
-					
+		List<Distance> distances = pcPO.getDistances();
+		int label1 = 0;// label表示状态，0表示未找到相同，1表示已找到相同
+		int label2 = 0;
+		for (Distance temp : distances) {
+			label1 = 0;
+			label2 = 0;
+			String city1 = temp.getCity1();
+			String city2 = temp.getCity2();
+			for (String tempCity : cities) {
+				if (tempCity.equals(city1)) {
+					// 如果找到相同的城市，label设置为1，跳出
+					label1 = 1;
+
 				}
-				if(tempCity.equals(city2)){
-					label2=1;
-					
+				if (tempCity.equals(city2)) {
+					label2 = 1;
+
 				}
-				if(label1==1||label2==1)
+				if (label1 == 1 && label2 == 1)
 					break;
 			}
-			//找到和未找到2中情况的判断
-			if(label1==1){
-				label1=0;
-			}else{
-				cities.add(city1);
+			// 找到和未找到2中情况的判断
+			if (label1 == 1 && label2 == 1) {
+				continue;
+			} else {
+				if (label1 == 1) {
+					label1 = 0;
+				} else {
+					cities.add(city1);
+				}
+
+				if (label2 == 1) {
+					label2 = 0;
+				} else {
+					cities.add(city2);
+				}
 			}
-			
-			if(label2==1){
-				label2=0;
-			}else{
-				cities.add(city2);
-			}
-			
-			
+
 		}
-		
+
 		return cities;
 	}
-	
-	public PriceConstVO getVO(){
-		if(pcPO!=null){
-			PriceConstVO vo=new PriceConstVO(pcPO);
+
+	public PriceConstVO getVO() {
+		if (pcPO != null) {
+			PriceConstVO vo = new PriceConstVO(pcPO);
 			return vo;
 		}
-		
+
 		return null;
 	}
-	
-	public String modify(PriceConstVO vo) throws RemoteException{
-		PriceConstPO po=vo.transToPO();
+
+	public String modify(PriceConstVO vo) throws RemoteException {
+		PriceConstPO po = vo.transToPO();
 		pcDS.update(po);
 		return null;
 	}
-	
+
 	public double getDistance(String city1, String city2) {
-		List<Distance> distances=pcPO.getDistances();
+		List<Distance> distances = pcPO.getDistances();
 		double result = 0;
-		Distance newD= new Distance(city1, city2,0);
-		
-		for(Distance temp: distances){
-//			if((temp.getCity1()==city1&&temp.getCity2()==city2)||(temp.getCity2()==city1&&temp.getCity1()==city2)){
-				if(newD.equals(temp)){
-					result=temp.getDistance();
-					return result;
-				}
+		Distance newD = new Distance(city1, city2, 0);
+
+		for (Distance temp : distances) {
+			// if((temp.getCity1()==city1&&temp.getCity2()==city2)||(temp.getCity2()==city1&&temp.getCity1()==city2)){
+			if (newD.equals(temp)) {
+				result = temp.getDistance();
+				return result;
+			}
 		}
-		
+
 		return result;
 	}
-	
 
-	public double calSendPrice(String city1, String city2, PackType packType, Express expressType) {
+	public double calSendPrice(String city1, String city2, PackType packType,
+			Express expressType) {
 		// TODO Auto-generated method stub
 		return 0;
 	}
@@ -110,7 +114,5 @@ public class StrategyBL {
 		// TODO Auto-generated method stub
 		return 0;
 	}
-	
-
 
 }
