51e01dadb15975e7b85858b010931248420894e9
wserious <948436185@qq.com>
2015-11-29 00:57:06 +0800

    SendBL添加计算运费（输入城市1，城市2，运送速度，包装种类，重量，尺寸等参数）

--- client/src/main/java/businessLogic/deliveryBL/SendBL.java
+++ client/src/main/java/businessLogic/deliveryBL/SendBL.java
@@ -1,13 +1,17 @@
 package businessLogic.deliveryBL;
 
 import java.rmi.RemoteException;
+import java.text.DecimalFormat;
 import java.util.List;
 
 import businessLogic.manageBL.StrategyBL;
+import po.PriceConstPO;
 import po.SendPO;
+import util.PackPrice;
 import vo.SendVO;
 import client.ClientInitException;
 import client.RMIHelper;
+import dataService.PriceConstDataService;
 import dataService.SendDataService;
 import enumSet.Express;
 import enumSet.PackType;
@@ -15,58 +19,92 @@ import enumSet.PackType;
 public class SendBL {
 	SendDataService sendDS;
 	List<SendPO> sendList;
-	
-	public  SendBL() throws RemoteException {
+
+	public SendBL() throws RemoteException {
 		try {
 			RMIHelper.initSendDataService();
 			sendDS = RMIHelper.getSendDataService();
-//			sendList=sendDS.();
+			sendList = sendDS.getSendList();
 		} catch (ClientInitException e) {
 			e.printStackTrace();
 		}
 	}
-	
-	public String add(SendVO vo) throws RemoteException{
-		SendPO po=vo.transToPO();
-		for(SendPO temp : sendList){
-			if(temp.getId().equals(po.getId()))
+
+	public String add(SendVO vo) throws RemoteException {
+		SendPO po = vo.transToPO();
+		for (SendPO temp : sendList) {
+			if (temp.getId().equals(po.getId()))
 				return "存在相同编号的快递单";
 		}
 		sendList.add(po);
 		sendDS.add(po);
-		
+
 		return null;
 	}
-	
-	public String delete(SendVO vo) throws RemoteException{
-		SendPO po=vo.transToPO();
-		for(SendPO temp: sendList){
-			if(temp.getId().equals(vo.getId())){
+
+	public String delete(SendVO vo) throws RemoteException {
+		SendPO po = vo.transToPO();
+		for (SendPO temp : sendList) {
+			if (temp.getId().equals(vo.getId())) {
 				sendList.remove(temp);
 				sendDS.delete(temp);
-				return null; 
+				return null;
 			}
 		}
-		
+
 		return "未找到对应快递单";
 	}
-	
-	
-	public double getPrice(String city1, String city2,PackType paceType,Express expressType,double weight) throws RemoteException{
-		//TODO  从StrategyBL 里得到距离，根据重量和速度计算时间，返回double类型数值
-		StrategyBL strategyBL=new StrategyBL();
-		double distance=strategyBL.getDistance(city1, city2);
-		double packFare=0;//包装费
-		double expressCoeff;//快递类型对应的系数，根据类型取不同的值
-		
-		for(Express temp: Express.values()){
-			if(temp==expressType){
-				expressCoeff=
-			}
+
+	public double getWeight(double weight,double length,double width, double height){
+		double result=0;
+		double newWeight=0;
+		newWeight=length*width*height;
+		if(weight>newWeight){
+			return weight;
+		}else{
+			return newWeight;
 		}
-		return 0;
 	}
 	
-	
+	public double getPrice(String city1, String city2, PackType packType,
+			Express expressType, double weight,double length,double width,double height) throws RemoteException,
+			ClientInitException {
+		RMIHelper.initPriceConstDataService();
+		PriceConstDataService pcDS = RMIHelper.getPriceConstDataService();
+		PriceConstPO pcPO = pcDS.getPriceConst();
+
+		// TODO 从StrategyBL 里得到距离，根据重量和速度计算时间，返回double类型数值
+		double result;
+		StrategyBL strategyBL = new StrategyBL();
+		double distance = strategyBL.getDistance(city1, city2);
+		double packFare = 0;// 包装费
+		double expressCoeff = 0;// 快递类型对应的系数，根据类型取不同的值
+
+		//计算运送种类不同对应的系数
+		if (expressType == Express.STANDARD) {
+			expressCoeff = pcPO.getStandard();
+		} else if (expressType == Express.FAST) {
+			expressCoeff = pcPO.getFast() ;
+		} else if (expressType == Express.ECONOMY) {
+			expressCoeff = pcPO.getEconomy();
+		}
+		//得到包装费
+		List<PackPrice> packs=pcPO.getPackPrice();
+		for(PackPrice temp : packs){
+			if(temp.getType()==packType){
+				packFare=temp.getFare();
+				break;
+			}
+		}
+		
+		weight=this.getWeight(weight,length,width,height);
+		
+		result=(distance/1000)*expressCoeff*weight+packFare;
+		//格式转化为2位小数
+		DecimalFormat df = new DecimalFormat("#.00");  
+		
+
+		return Double.parseDouble(df.format(result));
+	}
 
 }
--- client/src/main/java/businessLogic/deliveryBL/SendController.java
+++ client/src/main/java/businessLogic/deliveryBL/SendController.java
@@ -2,6 +2,9 @@ package businessLogic.deliveryBL;
 
 import java.rmi.RemoteException;
 
+import client.ClientInitException;
+import enumSet.Express;
+import enumSet.PackType;
 import vo.SendVO;
 import businessLogicService.deliveryBLService.SendBLService;
 
@@ -20,8 +23,8 @@ public class SendController implements SendBLService {
 	}
 //只有以上的有用
 
-	public double getFare(String departure, String destination, String packType, String TransferType) {
-		return 0;
+	public double getFare(String departure, String destination, PackType packType,Express  transferType,double weight,double length,double width,double height) throws RemoteException, ClientInitException {
+		return sendBL.getPrice(departure, destination, packType, transferType, weight,length,width,height);
 	}
 
 
@@ -32,6 +35,11 @@ public class SendController implements SendBLService {
 	public String delete() {
 		// TODO Auto-generated method stub
 		return null;
+	}
+	public double getFare(String departure, String destination,
+			String packType, String TransferType) {
+		// TODO Auto-generated method stub
+		return 0;
 	}
 	
 }
