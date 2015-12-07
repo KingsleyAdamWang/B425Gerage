04780871f651c465b176951c06e738ed4a75256d
KingsleyAdamWang <547212796@qq.com>
2015-12-07 15:44:40 +0800

    receiveImpl

--- server/src/main/java/dataServiceImpl/ReceiveDataServiceImpl.java
+++ server/src/main/java/dataServiceImpl/ReceiveDataServiceImpl.java
@@ -3,84 +3,120 @@ package dataServiceImpl;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import po.ReceivePO;
 import dataService.ReceiveDataService;
+import enumSet.ReceiptsState;
 
 public class ReceiveDataServiceImpl extends UnicastRemoteObject implements
 		ReceiveDataService {
-   
-	private File file= new File("src/main/java/data/Receive.txt");
-	private List<ReceivePO> receiveList ;
+
+	private File file = new File("src/main/java/data/Receive.txt");
+	private List<ReceivePO> receiveList;
 
 	public ReceiveDataServiceImpl() throws RemoteException {
 		super();
 		init();
 	}
 
-	
-	private void init(){
+	private void init() {
 		receiveList = new ArrayList<ReceivePO>();
 		try {
 			BufferedReader br = new BufferedReader(new FileReader(file));
+			String temp;
+			while ((temp = br.readLine()) != null) {
+				receiveList.add(new ReceivePO(temp));
+			}
+			br.close();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 	}
-	
-	
-	private void update(){
-		
+
+	private void update() {
+		try {
+			FileWriter fw = new FileWriter(file);
+			Iterator<ReceivePO> iterator = receiveList.iterator();
+			fw.write("");
+			while (iterator.hasNext()) {
+				fw.append(iterator.next().toString());
+				fw.flush();
+			}
+			fw.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
-	
+
 	@Override
 	public void add(ReceivePO po) throws RemoteException {
-		// TODO Auto-generated method stub
-		
+		receiveList.add(0, po);
+		update();
+
 	}
 
 	@Override
 	public void delete(ReceivePO po) throws RemoteException {
-		// TODO Auto-generated method stub
-		
+		if (receiveList.contains(po)) {
+			receiveList.remove(po);
+			update();
+		}
+
 	}
 
 	@Override
 	public void modify(ReceivePO po) throws RemoteException {
-		// TODO Auto-generated method stub
-		
+		if (receiveList.contains(po)) {
+			receiveList.set(receiveList.indexOf(po), po);
+			update();
+		}
+
 	}
 
 	@Override
 	public ReceivePO find(String id) throws RemoteException {
-		// TODO Auto-generated method stub
+		Iterator<ReceivePO> iterator = receiveList.iterator();
+
+		while (iterator.hasNext()) {
+			ReceivePO po = iterator.next();
+			if (po.getId().equals(id))
+				;
+			return po;
+		}
 		return null;
 	}
 
 	@Override
 	public void approve(ReceivePO po) throws RemoteException {
-		// TODO Auto-generated method stub
-		
+		receiveList.get(receiveList.indexOf(po))
+				.setState(ReceiptsState.approve);
+		update();
+
 	}
 
 	@Override
 	public void approveAll() throws RemoteException {
-		// TODO Auto-generated method stub
-		
+		Iterator<ReceivePO> iterator = receiveList.iterator();
+		while (iterator.hasNext()) {
+			iterator.next().setState(ReceiptsState.approve);
+		}
+		update();
 	}
 
 	@Override
 	public List<ReceivePO> getList() throws RemoteException {
-		// TODO Auto-generated method stub
-		return null;
+		return receiveList;
 	}
-
 
 }
