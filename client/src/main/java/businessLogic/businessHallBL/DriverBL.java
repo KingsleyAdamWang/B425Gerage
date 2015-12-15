3111e64e1732d20f60f7e6502492d37dbed9c8e7
wserious <948436185@qq.com>
2015-12-15 21:14:22 +0800

    no message

--- client/src/main/java/businessLogic/businessHallBL/DriverBL.java
+++ client/src/main/java/businessLogic/businessHallBL/DriverBL.java
@@ -4,6 +4,7 @@ import java.rmi.RemoteException;
 import java.util.List;
 
 import po.DriverPO;
+import vo.DriverVO;
 import client.ClientInitException;
 import client.RMIHelper;
 import dataService.DriverDataService;
@@ -16,12 +17,59 @@ public class DriverBL {
 		try {
 			RMIHelper.initDriverDataService();
 			driverDS = RMIHelper.getDriverDataService();
-			driverList = driverDS.;
+			driverList = driverDS.getList();
 		} catch (ClientInitException e) {
 			e.printStackTrace();
 		}
 	}
 	
-	public addDriver(Driver)
+	public DriverVO find(String id){
+		try {
+			return new DriverVO(driverDS.find(id));
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
 	
+	public String addDriver(DriverVO vo){
+		DriverPO po=vo.transtoPO();
+		
+		for(DriverPO temp:driverList){
+			if(temp.getId()==po.getId()){
+				return "存在相同编号";
+			}
+		}	
+		return null; 
+	}
+	
+	public String deleteDriver(DriverPO po){
+		for(DriverPO temp:driverList){
+			if(temp.getId().equals(po.getId())){
+				driverList.remove(temp);
+				try {
+					driverDS.delete(po);
+				} catch (RemoteException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		return null;
+	}
+	
+	public String modify(DriverPO po){
+		for(DriverPO temp:driverList){
+			if(temp.getId().equals(po.getId())){
+				try {
+					driverDS.delete(po);
+					driverList=driverDS.getList();
+					return null;
+				} catch (RemoteException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		
+		return "未找到对应司机";
+	}
 }
--- client/src/main/java/businessLogic/businessHallBL/DriverController.java
+++ client/src/main/java/businessLogic/businessHallBL/DriverController.java
@@ -1,7 +1,21 @@
 package businessLogic.businessHallBL;
 
+import java.rmi.RemoteException;
+
+import businessLogic.logBL.LogBL;
 import businessLogicService.businessHallBLService.DriverBLService;
 
 public class DriverController implements DriverBLService {
-
+	private DriverBL driverBL;
+	private LogBL logBL;
+	
+	public DriverController(){
+		try {
+			driverBL=new DriverBL();
+			logBL=new LogBL();
+		} catch (RemoteException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
