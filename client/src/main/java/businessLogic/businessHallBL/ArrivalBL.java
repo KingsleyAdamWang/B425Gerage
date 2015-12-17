23c2ae16b46242f36a19b005bd7733f5b5335c47
wserious <948436185@qq.com>
2015-12-17 23:51:58 +0800

    no message

--- client/src/main/java/businessLogic/businessHallBL/ArrivalBL.java
+++ client/src/main/java/businessLogic/businessHallBL/ArrivalBL.java
@@ -6,8 +6,10 @@ import java.util.List;
 
 import po.ArrivalPO;
 import po.TransferPO;
+import presentation.MainFrame;
 import vo.ArrivalVO;
 import businessLogic.logisticsBL.LogisticsBL;
+import businessLogic.manageBL.InstitutionBL;
 import client.ClientInitException;
 import client.RMIHelper;
 import dataService.ArrivalDataService;
@@ -17,7 +19,7 @@ import enumSet.ReceiptsState;
 public class ArrivalBL {
 	private ArrivalDataService arrivalDS;
 	private List<ArrivalPO> arrivalList;
-	
+
 	public ArrivalBL() throws RemoteException {
 		try {
 			RMIHelper.initArrivalDataService();
@@ -27,97 +29,106 @@ public class ArrivalBL {
 			e.printStackTrace();
 		}
 	}
-	
-//	public ArrivalVO newArrival(){
-//		ArrivalVO arrivalVO=new ArrivalVO(new ArrivalPO());
-//		arrivalVO.
-//	}
-	
-	public ArrivalVO search(String transferID){
-		for(ArrivalPO temp: arrivalList){
-			if(temp.getTransferId().equals(transferID)){
+
+	// public ArrivalVO newArrival(){
+	// ArrivalVO arrivalVO=new ArrivalVO(new ArrivalPO());
+	// arrivalVO.
+	// }
+
+	public ArrivalVO search(String transferID) {
+		for (ArrivalPO temp : arrivalList) {
+			if (temp.getTransferId().equals(transferID)) {
 				return new ArrivalVO(temp);
 			}
 		}
 		return null;
 	}
-	
-	public String add(ArrivalVO vo) throws RemoteException{
-		
-		ArrivalPO po=vo.transToPO();
-		
-		for(ArrivalPO temp:arrivalList){
-			if(temp.equals(po)){
+
+	public String add(ArrivalVO vo) throws RemoteException {
+
+		ArrivalPO po = vo.transToPO();
+
+		for (ArrivalPO temp : arrivalList) {
+			if (temp.equals(po)) {
 				return "存在相同到达单";
 			}
 		}
-		
+
 		po.setState(ReceiptsState.unapprove);
-		
+
 		arrivalList.add(po);
 		arrivalDS.add(po);
-		
-		//更新物流信息
-		//第一次的TransferID就是装车单，这些TransferID需要人工填入
-		List<String> goodsID=getIDList(po.getTransferId());
-		businessLogic.manageBL.InstitutionBL insBL=new businessLogic.manageBL.InstitutionBL();
-		String insName=insBL.searchInstitution(po.getInstitutionID()).getName();
-		updateLogistics(goodsID,insName);
+
+		// 更新物流信息
+		// 第一次的TransferID就是装车单，这些TransferID需要人工填入
+		List<String> goodsID = getIDList(po.getTransferId());
+		businessLogic.manageBL.InstitutionBL insBL = new businessLogic.manageBL.InstitutionBL();
+		String insName = insBL.searchInstitution(po.getInstitutionID())
+				.getName();
+		updateLogistics(goodsID, insName);
 		return null;
 	}
-	
-	public String updateLogistics(List<String> goodsID,String institutionName) throws RemoteException{
-		LogisticsBL logisticsBL=new LogisticsBL();
-		for(String temp: goodsID){
-			logisticsBL.update(temp, "到达"+institutionName);
+
+	public String updateLogistics(List<String> goodsID, String institutionName)
+			throws RemoteException {
+		LogisticsBL logisticsBL = new LogisticsBL();
+		for (String temp : goodsID) {
+			logisticsBL.update(temp, "到达" + institutionName);
 		}
 		return null;
-	} 
-	
-	public String delete(ArrivalVO vo) throws RemoteException{
-		ArrivalPO po=vo.transToPO();
-		
-		for(ArrivalPO temp: arrivalList){
-			if(temp.equals(po)){
+	}
+
+	public String delete(ArrivalVO vo) throws RemoteException {
+		ArrivalPO po = vo.transToPO();
+
+		for (ArrivalPO temp : arrivalList) {
+			if (temp.equals(po)) {
 				arrivalList.remove(temp);
 				arrivalDS.delete(po);
 			}
 		}
 		return "未找到到达单";
 	}
-	
-	public String modify(ArrivalVO vo) throws RemoteException{
-		ArrivalPO po=vo.transToPO();
-		
-		for(ArrivalPO temp : arrivalList){
-			if(temp.equals(po)){
+
+	public String modify(ArrivalVO vo) throws RemoteException {
+		ArrivalPO po = vo.transToPO();
+
+		for (ArrivalPO temp : arrivalList) {
+			if (temp.equals(po)) {
 				arrivalList.remove(temp);
 				arrivalList.add(po);
-				
+
 				arrivalDS.modify(po);
 				return null;
 			}
 		}
-		
+
 		return "未找到对应到达单";
 	}
-	
 
-	
-	//通过中转单单号查询对应的所有快递单号列表
-	public List<String> getIDList(String transferID) throws RemoteException{
-//		EntruckDataService entruckDS=new EntruckDataService();
-		
+	// 通过中转单单号查询对应的所有快递单号列表
+	public List<String> getIDList(String transferID) throws RemoteException {
+		// EntruckDataService entruckDS=new EntruckDataService();
+
 		try {
 			RMIHelper.initTransferDataService();
 			TransferDataService transferDS = RMIHelper.getTransferDataService();
-			TransferPO transferPO=transferDS.find(transferID);
+			TransferPO transferPO = transferDS.find(transferID);
 			return transferPO.getList();
-			//		List<EntruckPO> entruckList = entruckDS.getEntruckList();
+			// List<EntruckPO> entruckList = entruckDS.getEntruckList();
 		} catch (ClientInitException e) {
 			e.printStackTrace();
 		}
-		return null;	
+		return null;
+	}
+
+	public String getInstitutionName() throws RemoteException {
+		String result = null;
+
+		InstitutionBL institutionBL = new InstitutionBL();
+		result = institutionBL.searchInstitution(
+				MainFrame.getUser().getInstitutionID()).getName();
+		return result;
 	}
 
 	public void approve(ArrivalPO po) throws RemoteException {
@@ -129,9 +140,9 @@ public class ArrivalBL {
 	}
 
 	public List<ArrivalVO> getUnapproved() {
-		List<ArrivalVO> result=new ArrayList<ArrivalVO>();
-		for(ArrivalPO temp: arrivalList){
-			if(temp.getState()==ReceiptsState.unapprove){
+		List<ArrivalVO> result = new ArrayList<ArrivalVO>();
+		for (ArrivalPO temp : arrivalList) {
+			if (temp.getState() == ReceiptsState.unapprove) {
 				result.add(new ArrivalVO(temp));
 			}
 		}
--- client/src/main/java/businessLogic/businessHallBL/ArrivalController.java
+++ client/src/main/java/businessLogic/businessHallBL/ArrivalController.java
@@ -16,37 +16,44 @@ import dataService.LogDataService;
 public class ArrivalController implements ArrivalBLService {
 	private ArrivalBL arrivalBL;
 	private LogDataService logDS;
-	
-	public ArrivalController() throws RemoteException, ClientInitException{
-		arrivalBL=new ArrivalBL();
+
+	public ArrivalController() throws RemoteException, ClientInitException {
+		arrivalBL = new ArrivalBL();
 		RMIHelper.initLogDataService();
-		logDS=RMIHelper.getLogDataService();
+		logDS = RMIHelper.getLogDataService();
 	}
 
 	public String add(ArrivalVO arrivalVO) throws RemoteException {
-		logDS.add(new LogPO(new Date(), MainFrame.getUser().getIdentityID(), "新增到达单"));
+		logDS.add(new LogPO(new Date(), MainFrame.getUser().getIdentityID(),
+				"新增到达单"));
 		return arrivalBL.add(arrivalVO);
 	}
 
-
-	public ArrivalVO search(String transferID) throws RemoteException {//根据单号查找到达单
-		logDS.add(new LogPO(new Date(),MainFrame.getUser().getIdentityID(),"查询到达单"));
+	public ArrivalVO search(String transferID) throws RemoteException {// 根据单号查找到达单
+		logDS.add(new LogPO(new Date(), MainFrame.getUser().getIdentityID(),
+				"查询到达单"));
 		return arrivalBL.search(transferID);
 	}
 
-
 	public String delete(ArrivalVO arrivalVO) throws RemoteException {
-		logDS.add(new LogPO(new Date(), MainFrame.getUser().getIdentityID(), "删除到达单"));
+		logDS.add(new LogPO(new Date(), MainFrame.getUser().getIdentityID(),
+				"删除到达单"));
 		return arrivalBL.delete(arrivalVO);
 	}
-	
-	public String modify(ArrivalVO arrivalVO) throws RemoteException{
-		logDS.add(new LogPO(new Date(), MainFrame.getUser().getIdentityID(), "修改到达单"));
-		return arrivalBL.modify(arrivalVO); 
+
+	public String modify(ArrivalVO arrivalVO) throws RemoteException {
+		logDS.add(new LogPO(new Date(), MainFrame.getUser().getIdentityID(),
+				"修改到达单"));
+		return arrivalBL.modify(arrivalVO);
 	}
-	//根据装车单信息返回对应所有快递单号的列表
-	public List<String> getIDList(String entruckID) throws RemoteException{
+
+	// 根据装车单信息返回对应所有快递单号的列表
+	public List<String> getIDList(String entruckID) throws RemoteException {
 		return arrivalBL.getIDList(entruckID);
+	}
+
+	public String getInstitutionName() throws RemoteException {
+		return arrivalBL.getInstitutionName();
 	}
 
 }
