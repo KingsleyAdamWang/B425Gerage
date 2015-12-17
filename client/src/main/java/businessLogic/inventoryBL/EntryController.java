4944d860a3b3c403ae77e8e191b1f632f715722c
wserious <948436185@qq.com>
2015-12-17 22:58:10 +0800

    no message

--- client/src/main/java/businessLogic/inventoryBL/EntryController.java
+++ client/src/main/java/businessLogic/inventoryBL/EntryController.java
@@ -1,10 +1,39 @@
 package businessLogic.inventoryBL;
 
+import java.rmi.RemoteException;
+import java.util.List;
+
+import po.EntryPO;
 import vo.EntryVO;
 import businessLogicService.inventoryBLService.EntryBLService;
 
 public class EntryController implements EntryBLService {
-	public boolean submitEntry(EntryVO eVO){
+	EntryBL entryBL;
+	
+	public EntryController(){
+		try {
+			entryBL=new EntryBL();
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public String add(EntryVO vo) throws RemoteException{
+		return entryBL.add(vo);
+	}
+	
+	public String delete(EntryVO vo) throws RemoteException{
+		return entryBL.delete(vo);
+	}
+	
+	public List<EntryPO> getEntryList(String institutionID) throws RemoteException{
+		return entryBL.getEntryList(institutionID);
+	}
+
+	
+	
+	public boolean submitEntry(EntryVO eVO) {
+		// TODO Auto-generated method stub
 		return false;
 	}
 	
